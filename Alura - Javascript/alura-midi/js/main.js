9bf550e72cc23870f369b4ac87bf8cbce05e5a5a
root <root@Mari>
2023-07-06 18:01:56 -0300

    adding and refactoring functions.

--- Alura - Javascript/alura-midi/js/main.js
+++ Alura - Javascript/alura-midi/js/main.js
@@ -1,5 +1,11 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido!');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -15,7 +21,7 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     }
 
     tecla.onkeydown = function (evento) {
-        if(evento.code === "Enter" || evento.code === "Space") {
+        if (evento.code === "Enter" || evento.code === "Space") {
             tecla.classList.add('ativa');
         }
     }
